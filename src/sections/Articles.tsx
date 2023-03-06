34555491ed402d856d4aaa7cd8410a85948912fd
Jonathan <jalexisgarcia.91@gmail.com>
2023-03-06 16:25:34 -0300

    Portfolio

--- src/sections/Articles.tsx
+++ src/sections/Articles.tsx
@@ -15,7 +15,7 @@ export const Articles: FC<Props> = ({ isMobile, articleRef }) => {
     <section id="articles" ref={articleRef}>
       <Content>
         {isMobile ? <></> : <AboutShapes />}
-        <h1>Blogs</h1>
+        <h1>Blogs  of interest</h1>
         <ArticleWrapper>
           <ArticleCard
             description={
